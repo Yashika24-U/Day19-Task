ab2676fdb12dc47de7f07c7d78a4a74090acad0a
Yashika.U <97971705+Yashika24-U@users.noreply.github.com>
2023-02-06 02:00:32 +0530

    Add files via upload

---
+++ company.js
@@ -0,0 +1,29 @@
+const tablebody = document.querySelector('tbody');
+const fun1 = async (company) =>{
+    const response = await fetch(`https://companyenrichment.abstractapi.com/v1/?api_key=7c5f5dbea3b248b792b0c5a4a2d60ce0&domain=${company}.com`)
+    const value = await response.json();
+    console.log(value)
+    
+    const bodyrow = document.createElement('tr');
+    const bodycomp = document.createElement('td');
+    bodycomp.innerText = value.name
+    if(bodycomp.innerText == ""){
+        window.alert("company not present in API")
+    }
+    else{
+    const bodyFound = document.createElement('td');
+    bodyFound.innerText = value.year_founded
+    
+    const bodyHq = document.createElement('td');
+    bodyHq.innerText = value.country +` ( ${value.locality} )`
+    
+    const bodyDomain = document.createElement('td');
+    bodyDomain.innerText = value.domain
+    
+    const bodyempCount = document.createElement('td');
+    bodyempCount.innerText = value.employees_count
+        
+    bodyrow.append(bodycomp,bodyFound,bodyHq,bodyDomain,bodyempCount);
+    tablebody.append(bodyrow)
+    }
+}
